3a377cbadb7d8d83325af29a6956ea5a1acd144a
Valentyna Feshchenko <fesval777@gmail.com>
2023-01-15 19:24:39 +0100

    Check operation and print result

--- calc.py
+++ calc.py
@@ -3,3 +3,17 @@ if __name__ == "__main__":
 print(text)  
 number1=float(input("Введи перше число:")) 
 number2=float(input("Введи друге число:"))
+if text==1:
+  result=number1+number2
+  print(f"Сума чисел {number1} та {number2} дорівнює: {result}")
+elif text==2:
+  result=number1-number2
+  print(f"Різниця числа {number1} та числа {number2} дорівнює: {result}")
+elif text==3:
+  result=number1*number2
+  print(f"Множина чисел {number1} та {number2} дорівнює: {result}")
+elif text==4:
+  result=number1/number2
+  print(f"Частка числа {number1} та числа {number2} дорівнює: {result}")
+else:
+  print('Перевір,яку дію ввів, я таку не знаю')
